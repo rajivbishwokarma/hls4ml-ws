7ed43c6b59bb09e829c4b850c03d76eb3bff3548
Rajiv Bishwokarma <rajivbishwokarma@outlook.com>
2024-04-03 19:11:31 -0400

    intro complete

--- quickstart/quickstart.py
+++ quickstart/quickstart.py
@@ -4,3 +4,11 @@ config = hls4ml.utils.fetch_example_model('KERAS_3layer.json')
 print(config)
 
 hls_model = hls4ml.converters.keras_to_hls(config)
+
+hls4ml.utils.fetch_example_list()
+
+# Using vivado to synthesize the model
+hls_model.build()
+
+# print the report
+hls4ml.report.read_vivado_report('hls-report')
